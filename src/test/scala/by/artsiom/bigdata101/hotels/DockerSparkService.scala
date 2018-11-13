b04e7bd220d7ed6030cf5d74fc58f3cc7d8d05af
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-13 19:05:49 +0300

    add test

--- src/test/scala/by/artsiom/bigdata101/hotels/DockerSparkService.scala
+++ src/test/scala/by/artsiom/bigdata101/hotels/DockerSparkService.scala
@@ -1,5 +1,17 @@
 package by.artsiom.bigdata101.hotels
 
-trait DockerSparkService {
+import com.whisk.docker.scalatest.DockerTestKit
+import com.whisk.docker.{DockerContainer, DockerKit, DockerReadyChecker}
+import org.scalatest.Suite
 
+trait DockerSparkService extends DockerTestKit  { self: Suite =>
+
+  val pingContainer = DockerContainer("nginx:1.7.11")
+
+  val pongContainer = DockerContainer("nginx:1.7.11")
+    .withPorts(80 -> None)
+    .withReadyChecker(
+      DockerReadyChecker.HttpResponseCode(port = 80, path = "/", host = None, code = 200))
+
+  override def dockerContainers = pingContainer :: pongContainer :: super.dockerContainers
 }
--- src/test/scala/by/artsiom/bigdata101/hotels/PingPongSpec.scala
+++ src/test/scala/by/artsiom/bigdata101/hotels/PingPongSpec.scala
@@ -1,5 +1,14 @@
 package by.artsiom.bigdata101.hotels
 
-class PingPongSpec {
+import com.whisk.docker.impl.spotify.DockerKitSpotify
+import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.time.{Second, Seconds, Span}
 
+class PingPongSpec extends FlatSpec with Matchers with DockerKitSpotify with DockerSparkService {
+  implicit val pc = PatienceConfig(Span(20, Seconds), Span(1, Second))
+
+  "all containers" should "be ready at the same time" in {
+    dockerContainers.map(_.image).foreach(println)
+    dockerContainers.forall(c => isContainerReady(c).futureValue) shouldBe true
+  }
 }
