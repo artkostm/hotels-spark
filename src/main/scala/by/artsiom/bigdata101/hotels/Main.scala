5c01d1b9beeaf6f4f1b28a2e1fdf1a81ca49e072
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-14 18:12:30 +0300

    add file schema

--- src/main/scala/by/artsiom/bigdata101/hotels/Main.scala
+++ src/main/scala/by/artsiom/bigdata101/hotels/Main.scala
@@ -1,27 +1,22 @@
 package by.artsiom.bigdata101.hotels
 
-import java.net.URI
-
 import org.apache.spark.sql.SparkSession
 
 object Main extends App with Homework {
 
   val spark = SparkSession.builder
-    .master(s"spark://${new URI(sys.env("DOCKER_HOST")).getHost}:7077")
+    .master("local[*]")
     .appName("hotels")
     .getOrCreate()
 
   implicit val test = spark.read
     .option("header", "true")
-    .option("inferSchema", "true")
-    //.schema(schema)
-    .csv("src/test/resources/test.csv") //D:\homework\Hadoop.Intro\train.csv
+    .schema(FileSchema)
+    .csv("src/test/resources/test.csv")
 
-  task1.explain(true)
-
-//  task2.explain(true)
-//
-//  task3.explain(true)
+  task3.show()
+  task3.show()
+  task3.show()
 
   spark.stop()
 }
