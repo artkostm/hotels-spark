d9206db9645a8209d9934f213615a8475ef2dcfc
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-14 14:17:22 +0300

    added containers for spark cluster with one master and two workers

--- src/test/scala/by/artsiom/bigdata101/hotels/DockerSparkService.scala
+++ src/test/scala/by/artsiom/bigdata101/hotels/DockerSparkService.scala
@@ -1,17 +1,37 @@
 package by.artsiom.bigdata101.hotels
 
+import java.net.URI
+
 import com.whisk.docker.scalatest.DockerTestKit
-import com.whisk.docker.{DockerContainer, DockerKit, DockerReadyChecker}
+import com.whisk.docker.{ContainerLink, DockerContainer, DockerKit, DockerReadyChecker}
 import org.scalatest.Suite
 
+import scala.concurrent.duration
+import scala.concurrent.duration.FiniteDuration
+
+/**
+  * Deploys 3 containers: one spark master and two spark workers
+  */
 trait DockerSparkService extends DockerTestKit  { self: Suite =>
 
-  val pingContainer = DockerContainer("nginx:1.7.11")
+  val host = new URI(sys.env("DOCKER_HOST")).getHost
 
-  val pongContainer = DockerContainer("nginx:1.7.11")
-    .withPorts(80 -> None)
+  val master = DockerContainer(image = "bde2020/spark-master:2.3.2-hadoop2.7", name = Some("spark-master"))
+    .withPorts(8080 -> Some(8080), 7077 -> Some(7077))
+    .withEnv("INIT_DAEMON_STEP=setup_spark")
     .withReadyChecker(
-      DockerReadyChecker.HttpResponseCode(port = 80, path = "/", host = None, code = 200))
+      DockerReadyChecker.HttpResponseCode(port = 8080, path = "/", host = Some(host), code = 200)
+        .looped(3, FiniteDuration(15, duration.SECONDS)))
 
-  override def dockerContainers = pingContainer :: pongContainer :: super.dockerContainers
+  val worker1 = DockerContainer(image = "bde2020/spark-worker:2.3.2-hadoop2.7", name = Some("spark-worker1"))
+    .withPorts(8081 -> Some(8081))
+    .withEnv(s"SPARK_MASTER=spark://$host:7077")
+    .withLinks(ContainerLink(master, "spark-master"))
+
+  val worker2 = DockerContainer(image = "bde2020/spark-worker:2.3.2-hadoop2.7", name = Some("spark-worker2"))
+    .withPorts(8081 -> Some(8081))
+    .withEnv(s"SPARK_MASTER=spark://$host:7077")
+    .withLinks(ContainerLink(master, "spark-master"))
+
+  override def dockerContainers = master :: worker1 :: worker2 :: super.dockerContainers
 }
