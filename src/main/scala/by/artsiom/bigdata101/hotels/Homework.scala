eb7b432b42be99e56ee4868348ae5518aee50cd1
Artsiom Chuiko <Artsiom_Chuiko@epam.com>
2018-11-14 18:13:08 +0300

    add comments

--- src/main/scala/by/artsiom/bigdata101/hotels/Homework.scala
+++ src/main/scala/by/artsiom/bigdata101/hotels/Homework.scala
@@ -6,9 +6,19 @@ import org.apache.spark.sql.functions.col
 trait Homework {
   import Columns._
 
+  /**
+    * Find top 3 most popular hotels between couples. (treat hotel as composite key of continent, country and market).
+    * @param df
+    * @return DataFrame, schema: [hotel_continent: int, hotel_country: int, hotel_market: int, count: long]
+    */
   def task1(implicit df: DataFrame): DataFrame =
     popularHotels(df, s"$AdultsCount = 2", 3)
 
+  /**
+    * Find the most popular country where hotels are booked and searched from the same country.
+    * @param df
+    * @return DataFrame, schema: [hotel_country: int, count: long]
+    */
   def task2(implicit df: DataFrame): DataFrame =
     df.where(s"$IsBooking = 1 AND $UserCountry = $HotelCountry")
       .groupBy(col(HotelCountry))
@@ -16,6 +26,11 @@ trait Homework {
       .orderBy(col(Count).desc)
       .limit(1)
 
+  /**
+    * Find top 3 hotels where people with children are interested but not booked in the end.
+    * @param df
+    * @return DataFrame, schema: [hotel_continent: int, hotel_country: int, hotel_market: int, count: long]
+    */
   def task3(implicit df: DataFrame): DataFrame =
     popularHotels(df, s"$ChildrenCount > 0 AND $IsBooking = 0", 3)
 
